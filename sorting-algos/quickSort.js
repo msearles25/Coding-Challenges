701d6c6b87b7a02acdc9ec8a74e3c4da99a4b156
Michael <skaman4459@gmail.com>
2020-06-05 17:44:38 -0400

    quick sort algo

---
+++ sorting-algos/quickSort.js
@@ -0,0 +1,20 @@
+const quickSort = (arr) => {
+    if(arr.length <= 1) return arr;
+
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+
+    for(let i = 0; i < arr.length - 1; i++) {
+        if(arr[i] < pivot) {
+            left.push(arr[i]);
+            continue;
+        }
+        right.push(arr[i]);
+    }
+
+
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+console.log(quickSort([10, 8, 2, 1, 6, 3, 9, 4, 7, 5]))
